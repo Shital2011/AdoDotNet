75df21f556807de795953e4c94eebd13457345c3
Shital2011 <shitalrkoli2011@gmail.com>
2022-08-24 12:32:47 +0530

    Add project files.

---
+++ LinQ_Demos/Demo_Object_linq.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoDotNet.LinQ_Demos
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string CompanyName { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} -> {Name} -> {Price} -> {CompanyName}";
+        }
+    }
+
+    class Demo_Object_linq
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            new Product { Id = 1, Name = "Mouse", Price = 799, CompanyName = "Dell" };
+            new Product { Id = 2, Name = "Mouse", Price = 900, CompanyName = "Dell" };
+            new Product { Id = 3, Name = "Mouse", Price = 799, CompanyName = "Dell" };
+            new Product { Id = 4, Name = "Mouse", Price = 799, CompanyName = "Dell" };
+            new Product { Id = 5, Name = "Mouse", Price = 799, CompanyName = "Dell" };
+            new Product { Id = 6, Name = "Mouse", Price = 799, CompanyName = "Dell" };
+            new Product { Id = 7, Name = "Mouse", Price = 799, CompanyName = "Dell" };
+            new Product { Id = 8, Name = "Mouse", Price = 799, CompanyName = "Dell" };
+            new Product { Id = 9, Name = "Mouse", Price = 799, CompanyName = "Dell" };
+            new Product { Id = 10, Name = "Mouse", Price = 799, CompanyName = "Dell" };
+
+
+
+            var result1 = from p in products
+                          select p;
+
+            var result2 = from p in products
+                          where p.Price < 2000
+                          select p;
+
+            var result3 = from p in products
+                          where p.Price > 2000 && p.Price < 3000
+                          select p;
+
+            //var result4 = from p in products
+            //              where p.CompanyName.StartsWith("D")
+            //              select p;
+            //var result4 = from p in products
+            //              where p.CompanyName.EndsWith("o")
+            //              select p;
+            var result4 = from p in products
+                          where p.CompanyName.Contains("m") || p.CompanyName.Contains("M")
+                          select p;
+            var result5 = from p in products
+                          where p.Price < 2500
+                          orderby p.Price descending
+                          select p;
+            foreach(Product item in result4)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        
+        public string City { get; set; }
+        public override string ToString()
+        {
+            return $"{Id} -->{Name}--> {Salary} -->{City} ";
+        }
+
+    }
+    public class Assign_LinqToObject
+    {
+
+        static void Main(string[] args)
+        {
+            List<Employee> Emp = new List<Employee>()
+            {
+
+
+            new Employee { Id = 1, Name = "shital", Salary = 30000, City = "Kolhapur"},
+            new Employee { Id = 2, Name = "sanket", Salary = 40000, City = "pune" },
+            new Employee { Id = 3, Name = "saurav", Salary = 35000, City = "Solapur" },
+            new Employee { Id = 4, Name = "manoj", Salary = 40000, City = "Ich" },
+            new Employee { Id = 5, Name = "kiran", Salary = 50000, City = "pune" },
+            new Employee { Id = 6, Name = "Rakesh", Salary = 30000, City = "mumbai" },
+            new Employee { Id = 7, Name = "aniket", Salary = 45000, City = "Ich" },
+            new Employee { Id = 8, Name = "Sanjay", Salary = 20000, City = "Kolhapur" },
+            };
+            //Display all emplyees
+            var result1 = from p in Emp
+                          select p;
+            
+            //Display employee with ascending order
+            var result2 = from p in Emp
+                          orderby p.Name 
+                          select p;
+            //display Employees whose salary is >25000
+            var result3 = from p in Emp
+                          where p.Salary > 25000
+                          select p;
+            //display Employees Whose city is "Pune"
+            var result4 = from p in Emp
+                          where p.City == "Pune"
+                          select p;
+            //display employee with desending order by salary
+            var result5 = from p in Emp
+                          orderby p.Salary descending
+                          select p;
+            //display employee whose name start with 'S'
+            var result6 = from p in Emp
+                          orderby p.Name.StartsWith("S")
+                          select p;
+            //display employee whose salary is > 25000 & emp is from 'Mumbai' city
+            var result7 = from p in Emp
+                          where (p.Salary > 25000) && (p.City == "mumbai")
+                          select p;
+            foreach (Employee item in result7)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+        
+}
---
+++ WithoutConn/WithoutConn.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace AdoDotNet.WithoutConn
+{
+    static class DBconnect
+    {
+        public static SqlConnection getConnection()
+        {
+            string str = "server=DESKTOP-46PU1N7;Database=ThinkQutient;Integrated Security=True";
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection(str);
+                con.Open();
+                SqlCommand sqlc = new SqlCommand("select*from newemp2", con);
+                SqlDataReader dr = sqlc.ExecuteReader();
+                while (dr.Read())
+                {
+                    Console.WriteLine("id==" + dr[0] + " " + "name==" + dr[1] + " " + "salary==" + dr[2] + " " + "city==" + dr[3] + "Email==" + dr[4]);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (con != null)
+                    con.Close();
+
+            }
+            
+
+
+            return con;
+        }
+    }
+
+    class DisConnected
+    {
+        static SqlDataAdapter sda = null;
+        static DataSet ds = null;
+        public static DataSet getallstudent()
+        {
+            SqlConnection con = null;
+            con = DBconnect.getConnection();
+            sda = new SqlDataAdapter("select * from newemp2", con);
+            //to assign primary key to col in dataset
+            sda.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            ds = new DataSet();//collection of tables
+            sda.Fill(ds);
+            return ds;
+
+        }
+        static public void Addstudent()
+        {
+            Console.WriteLine("enter id,nm,sal,city,email");
+            int id = int.Parse(Console.ReadLine());
+            string name = Console.ReadLine();
+            int sal = int.Parse(Console.ReadLine());
+            string city = Console.ReadLine();
+            string email = Console.ReadLine();
+            SqlCommandBuilder scb = new SqlCommandBuilder(sda);
+            DataRow dr = ds.Tables[0].NewRow();
+            dr[0] = id;
+            dr[1] = name;
+            dr[2] = sal;
+            dr[3] = city;
+            dr[4] = email;
+            ds.Tables[0].Rows.Add(dr);
+
+
+        }
+        public static void showAllstudent()
+        {
+            getallstudent();
+            foreach(DataRow dr in ds.Tables[0].Rows)
+            {
+                Console.WriteLine(dr[0] + " " + dr[1] + " " + dr[2]+" "+dr[3]+" "+dr[4]);
+            }
+            
+        }
+        static public void Updatestudent()
+        {
+            Console.WriteLine("enter id to update");
+            int id = int.Parse(Console.ReadLine());
+            SqlCommandBuilder scb = new SqlCommandBuilder(sda);
+            DataRow dr = ds.Tables[0].Rows.Find(id);
+            Console.WriteLine("Find   " + dr[0] + " " + dr[1] + " " + dr[2]);
+            Console.WriteLine("Enter new name");
+            dr[1] = Console.ReadLine();
+            dr[2] = 12;
+        }
+        public static void deletestudent()
+        {
+            SqlCommandBuilder scb = new SqlCommandBuilder(sda);
+            Console.WriteLine("Enter id to delete");
+            int id = int.Parse(Console.ReadLine());
+            DataRow dr = ds.Tables[0].Rows.Find(id);
+            if (dr != null)
+                dr.Delete();
+        }
+        static void Main(string[] args)
+        {
+            getallstudent();
+            Addstudent();
+            showAllstudent();
+            getallstudent();
+            Updatestudent();
+            deletestudent();
+           
+            Console.ReadLine();
+        }
+        
+
+     
+    }
+    
+}
